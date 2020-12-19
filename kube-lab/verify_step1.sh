32a0036786ea588040b2c7a7ef2b20ed243c49fe
Erik Kringen <ekringen@outlook.com>
2020-12-18 23:30:00 -0500

    Added verification logic

--- kube-lab/verify_step1.sh
+++ kube-lab/verify_step1.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-echo "done"
+
+NAMESPACE="jakku"
+POD="web"
+
+# Verify namespace exists
+kubectl get namespace $NAMESPACE > /dev/null 2>&1
+
+if [ $? != 0 ]
+    then
+        echo "Namespace does not exist"
+    else
+        echo "done"
+fi
+
+# check if pod exists
+kubectl get pod -n $NAMESPACE | cut -f 1 -d ' ' | grep $POD
+
+if [ $? != 0 ]
+    then
+        echo "Pod does not exist"
+    else
+        echo "done"
+fi
